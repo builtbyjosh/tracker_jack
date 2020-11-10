bb9d6b7c0c9d3cc12a9954eb0591e258870f580b
builtbyjosh <admin@builtbyjosh.com>
2020-11-09 19:32:07 -0500

    added user auth to children controller

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -2,50 +2,95 @@ class ChildrenController < ApplicationController
 
   # GET: /children
   get "/children" do
-    @child = current_user.children.all
-    erb :"/children/index_child"
+    if logged_in?
+      @child = current_user.children.all
+      erb :"/children/index_child"
+    else
+      redirect to '/login'
+    end
   end
 
   # GET: /children/new
   get "/children/new" do
-    erb :"/children/new_child"
+    if logged_in?
+      erb :"/children/new_child"
+    else
+      redirect to '/login'
+    end
   end
 
   # POST: /children
   post "/children" do
-    if params[:name] == "" || params[:dob] == ""
-      redirect to '/children/new'
+    if logged_in?
+      if params[:name] == "" || params[:dob] == ""
+        redirect to '/children/new'
+      else
+        @child = current_user.children.create(:name => params[:name], :dob => params[:dob])      
+        redirect to '/children'
+      end
     else
-      @child = current_user.children.create(:name => params[:name], :dob => params[:dob])      
-      redirect to '/children'
+      redirect to '/login'
     end
   end
 
   get '/children/:slug' do
-    @child = Child.find_by_slug(params[:slug])
-    if @child.appointment != nil 
-      @appointment = @child.appointment.last
+    if logged_in?
+      @child = Child.find_by_slug(params[:slug])
+      if @child.appointment != nil 
+        @appointment = @child.appointment.last
+      end
+      if @child.milestones != nil
+        @milestone = @child.milestones.last
+      end
+      erb :'children/show_child'
+    else
+      redirect to '/login'
     end
-    if @child.milestones != nil
-      @milestone = @child.milestones.last
-    end
-    erb :'children/show_child'
   end
 
   get '/children/:slug/edit' do
-    @child = Child.find_by_slug(params[:slug])
-    erb :"/children/edit_child"
+    if logged_in?
+      @child = Child.find_by_slug(params[:slug])
+      if @child && @child.parent == current_user
+        erb :"/children/edit_child"
+      else
+        redirect to '/children'
+      end
+    else
+      redirect '/login'
+    end
   end
 
   patch '/children/:slug' do
-    @child = Child.find_by_slug(params[:slug])
-    @child.update(name: params[:name], dob: params[:dob])
-    redirect to "/children/#{@child.name.downcase}"
-  end  
+    if logged_in?
+      if params[:name] == "" || params[:dob] == ""
+        redirect to "/children/#{:slug}"
+      else
+        @child = Child.find_by_slug(params[:slug])
+        if @child && @child.parent == current_user
+          if @child.update(name: params[:name], dob: params[:dob])
+            redirect to "/children/#{@child.name.downcase}"
+          else
+            redirect to "/children/#{@child.name.downcase}/edit"
+          end
+        else
+          redirect to '/children'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
 
   delete '/children/:slug/delete' do
-    @child = Child.find_by_slug(params[:slug])
-    @child.delete
-    redirect to '/children'
+    if logged_in?
+      @child = Child.find_by_slug(params[:slug])
+      if @child && @child.parent == current_user
+        @child.delete
+      end
+      redirect to '/children'
+    else
+      redirect to '/login'
+    end
   end
 end
